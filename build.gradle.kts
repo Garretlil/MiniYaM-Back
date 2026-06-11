77ab7b777ea63a657f92d365d2aa25589b5fd4e8
Garretlil <ed763135@gmail.com>
2026-06-11 10:16:47 +0300

    Added likes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.6"
-	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "1.9.10"
+	kotlin("plugin.spring") version "1.9.10"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.example"
@@ -11,7 +11,7 @@ description = "Demo project for Spring Boot"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion.set(JavaLanguageVersion.of(17))
 	}
 }
 
@@ -20,29 +20,29 @@ repositories {
 }
 
 dependencies {
-	//implementation("org.flywaydb:flyway-core:9.23.1")
-	implementation("org.postgresql:postgresql")
-
-	implementation("org.springframework.boot:spring-boot-starter")
+	// Spring Web
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	// Spring JPA + база (H2 для теста)
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
+	// Jackson
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	//implementation("org.springframework.session:spring-session-jdbc")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	//developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// BCrypt
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	// Mp3
 	implementation("com.mpatric:mp3agic:0.9.1")
+	// Тесты
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa") // JPA/Hibernate
-	runtimeOnly("org.postgresql:postgresql:42.6.0")
-	implementation("org.springframework.security:spring-security-crypto:5.7.1")
-	implementation("at.favre.lib:bcrypt:0.9.0")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+	kotlinOptions.jvmTarget = "17"
+	kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
 }
 
 tasks.withType<Test> {
